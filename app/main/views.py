234c7a34a05a15bffd080f4658b3d99f29098e34
StarlingNiohuru <Sider@qq.com>
2017-06-26 18:51:33 +0800

    some modified in js

--- app/main/views.py
+++ app/main/views.py
@@ -2,7 +2,7 @@ from flask import render_template, url_for, jsonify, request
 from flask_login import login_required, current_user
 from werkzeug.utils import redirect
 
-from app.main.forms import ShowEntriesForm, EditEntryForm
+from app.main.forms import ShowEntriesForm, EditEntryForm, EditDefinitionForm
 from app.main.models import Entry
 from config import ENTRY_TYPE_CHOICE
 from . import main
@@ -27,8 +27,9 @@ def dictionary():
                                  last_modified__lte=form.last_modified_to.data,
                                  entry_type__in=entry_type_list)
     edit_entry_form = EditEntryForm()
+    edit_def_form = EditDefinitionForm()
     return render_template('dictionary.html', form=form, query=query, entry_type_dict=dict(ENTRY_TYPE_CHOICE),
-                           edit_entry_form=edit_entry_form)
+                           edit_entry_form=edit_entry_form, edit_def_form=edit_def_form)
 
 
 @main.route('/edit', methods=['POST'])
